fd4e37aa6783312c611d1b3752b2933ce7493ff1
Cutoffdragon <jasonlagrasse@gmail.com>
2024-05-21 20:56:47 -0400

    FIX: start script

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ apiRoutes(app);
 routes(app);
 
 //Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT, "0.0.0.0", function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
